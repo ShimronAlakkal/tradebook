78fe2f4e20bb8ce22d9e4f55020b8079939360f0
Shimron <60049707+ShimronAlakkal@users.noreply.github.com>
2021-10-05 12:30:05 +0530

    new files up. Starting off again

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,2 @@
 import 'package:flutter/material.dart';
-import 'package:appa/Views/HomePage.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: 'Sample App for procedure',
-      home: InfoPage(),
-    ),
-  );
-}
