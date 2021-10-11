2767b3c2f20cde0a78e44909ad8d8f185d483819
Shimron <60049707+ShimronAlakkal@users.noreply.github.com>
2021-10-11 19:00:42 +0530

    Add files via upload

---
+++ lib/tools/positionSize.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PositionSizer extends StatefulWidget {
+  const PositionSizer({Key key}) : super(key: key);
+
+  @override
+  _PositionSizerState createState() => _PositionSizerState();
+}
+
+class _PositionSizerState extends State<PositionSizer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
