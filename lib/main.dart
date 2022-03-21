2803a5d9f82edb0313df6efe918d64f387874f07
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2022-03-21 10:28:21 +0530

    mobile ads integration after launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fraction/theme/theme.dart';
 import 'package:fraction/views/home.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
 
   runApp(
     MaterialApp(
